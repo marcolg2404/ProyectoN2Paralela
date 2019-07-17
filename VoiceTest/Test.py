62c36a7686d3430f4b68ba572d8cb8021c66e084
Sebastian Olivares <50059286+Sebaolivares11@users.noreply.github.com>
2019-07-16 20:03:08 -0400

    Se crea la funcion por rango
    usando rangos aproximados de frecuencia fundamental

--- VoiceTest/Test.py
+++ VoiceTest/Test.py
@@ -1,15 +1,40 @@
 mysp=__import__("my-voice-analysis") # importamos la libreria de python
+#funcion que retornara el rango etario y el genero de una persona
+def Rango( genero ,frecuencia):
+    if(genero == 'hombre'):
+        if (frecuencia <= 121.5 and frecuencia >110):
+            print("es Hombre Adolecente")
+        if(frecuencia > 121.95 and frecuencia <=127):
+            print("es Hombre Adulto")
+        #encontrar la relacion
+        if(frecuencia > 127.5 and frecuencia < 164):
+            print("es Hombre anciano")
+        elif (frecuencia > 155):
+            print("es niño Hombre")
+    if (genero == 'mujer'):
+        if (frecuencia <= 224.58 and frecuencia >196.31):
+            print("es Mujer Adolecente")
+        if(frecuencia <=196.31  and frecuencia >178.92):
+            print("es Mujer Adulto")
+        #encontrar la relacion
+        if(frecuencia <= 178.92 ):
+            print("es Mujer anciana")
+        elif (frecuencia > 200):
+            print("es niña ")
 
-p="female01" #nombre del archivo wav
+
+
+p="male001" #nombre del archivo wav
 c=r"/home/seba/Escritorio/VoiceTest/" #ubicacion de la carpeta
 
-x=mysp.myspgend(p,c)
-y=mysp.myspf0med(p,c)
+x=mysp.myspgend(p,c) #asignamos el genero
+y=mysp.myspf0med(p,c) #asignamos la frecuencia fundamental
+Rango(x,y)
 
 
-print(x)
-print(y)
 
+
+        #encontrar la relacion
 #frecuencia fundamental global
 #mysp.myspf0min(p,c)#frecuencia fundamental global minima
 #mysp.myspf0max(p,c)#frecuencia fundamental global maxima
